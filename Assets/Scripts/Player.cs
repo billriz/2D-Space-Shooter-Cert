4b13a032160b7ced0a47ecd8723401f9fef1b863
billriz <billarislov@gmail.com>
2021-04-04 20:49:55 -0500

    Enimies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
-
-    private float _fireRate = .15f;
+    [SerializeField]
+    private float _fireRate = .50f;
 
     private bool _canFire = true;
 
+    
     [SerializeField]
     private GameObject _laserPrefab;
-    
+    [SerializeField]
+    private int _lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,58 +31,73 @@ public class Player : MonoBehaviour
 
         CalculateMovement();
 
-        // when space key is press
-        if (Input.GetKeyDown(KeyCode.Space) && _canFire)
-        {
+        FireLaser();
 
-            // fire laser at the position of the player
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, +.85f, 0), Quaternion.identity);
-            _canFire = false;
-            StartCoroutine(FireControlTimer());
-
-
-
-        }
+       
     }
 
-        void CalculateMovement()
+    void CalculateMovement()
     {
      
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+           float horizontalInput = Input.GetAxis("Horizontal");
+           float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * _speed * Time.deltaTime);
+           Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+           transform.Translate(direction * _speed * Time.deltaTime);
 
-        if(transform.position.y >= 0)
-        {
+          if(transform.position.y >= 0)
+          {
             transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if(transform.position.y <= -5.8f)
-        {
+          }
+          else if(transform.position.y <= -5.8f)
+          {
             transform.position = new Vector3(transform.position.x, -5.8f, 0);           
-        }
+          }
 
-        if (transform.position.x >= 12.9f)
-        {
+          if (transform.position.x >= 12.9f)
+          {
             transform.position = new Vector3(-12.9f, transform.position.y, 0);
-        }
-        else if (transform.position.x <= -12.9f)
-        {
+          }
+          else if (transform.position.x <= -12.9f)
+          {
             transform.position = new Vector3(12.9f, transform.position.y, 0);
-        }
+          }
            
 
     } 
 
+    void FireLaser()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Space) && _canFire)
+        {
+
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, +.85f, 0), Quaternion.identity);
+            _canFire = false;           
+            StartCoroutine(FireControlTimer());
+
+        }
+
+    }
 
     IEnumerator FireControlTimer()
     {
 
-        yield return new WaitForSeconds(_fireRate);
-        _canFire = true;
+          yield return new WaitForSeconds(_fireRate);
+          _canFire = true;
 
 
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if (_lives < 1)
+        {
+
+            Destroy(this.gameObject);
+        }
     }
 
 
